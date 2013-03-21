6bf76d2069d12867dbca48c8f8f48d14d4864976
apawlicka <pawlicka.anna@gmail.com>
2013-03-21 12:18:28 +0000

    Logging fix: Added commas after each individual's fitness

--- controllers/SupervisorController/utils/FilesFunctions.java
+++ controllers/SupervisorController/utils/FilesFunctions.java
@@ -46,7 +46,7 @@ public class FilesFunctions {
     public static void logAllFitnesses(BufferedWriter out2, int generation, double[] fitnesses){
         try{
             out2.write(""+generation+", ");
-            for(int i=0; i<fitnesses.length; i++) out2.write(""+fitnesses[i]);
+            for(int i=0; i<fitnesses.length; i++) out2.write(""+fitnesses[i]+", ");
             out2.write("\n");
             out2.flush();
         } catch (IOException ioe){
