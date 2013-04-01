42ce29d40880b693195e25f482c672bb7f22960e
apawlicka <pawlicka.anna@gmail.com>
2013-04-01 17:21:54 +0200

    Added methods to calcualate max_in and min_int for fitness score normalization (avoiding external libs).

--- controllers/SupervisorController/utils/Util.java
+++ controllers/SupervisorController/utils/Util.java
@@ -17,6 +17,7 @@ public class Util {
      * Y = normpdf(X,mu,sigma) computes the pdf at each of the values in X using
      * the normal distribution with mean mu and standard deviation sigma.
      * X, mu, and sigma can be vectors, matrices, or multidimensional arrays that all have the same size.
+     *
      * @param x
      * @param mean
      * @param sd
@@ -30,6 +31,7 @@ public class Util {
 
     /**
      * Product of array elements
+     *
      * @param a
      * @return
      */
@@ -44,6 +46,7 @@ public class Util {
 
     /**
      * Copy one array into another
+     *
      * @param a
      * @return
      */
@@ -68,15 +71,14 @@ public class Util {
      * Modulus after division
      * M = mod(X,Y) if Y ~= 0, returns X - n.*Y where n = floor(X./Y).
      * If Y is not an integer and the quotient X./Y is within roundoff error of an integer, then n is that integer.
+     *
      * @param x
      * @param y
      * @return
      */
-    public static double mod(double x, double y)
-    {
+    public static double mod(double x, double y) {
         double result = x % y;
-        if (result < 0)
-        {
+        if (result < 0) {
             result += y;
         }
         return result;
@@ -84,27 +86,29 @@ public class Util {
 
     /**
      * Create an empty binary string of given length
+     *
      * @param x
      * @return
      */
-    public static String getEmptyBinaryString(int x){
+    public static String getEmptyBinaryString(int x) {
 
         String str = "0";
-        for(int i=0; i<x-1; i++){
-           str += "0";
+        for (int i = 0; i < x - 1; i++) {
+            str += "0";
         }
         return str;
     }
 
     /**
      * Computes the arithmetic mean of a set of values. Uses the definitional formula: mean = sum(x_i) / n
-     * @param values    An array of doubles
-     * @return          Arithmetic mean (double)
+     *
+     * @param values An array of doubles
+     * @return Arithmetic mean (double)
      */
-    public static double mean(double[] values){
+    public static double mean(double[] values) {
         double sum = 0.0;
-        for(int i=0; i<values.length; i++){
-            sum+=values[i];
+        for (int i = 0; i < values.length; i++) {
+            sum += values[i];
         }
         return (sum) / values.length;
     }
@@ -115,8 +119,8 @@ public class Util {
      * @param min   lowest saturation point. Values below this will return 0
      * @param max   highest saturation point. Values above this will return 1
      * @param value
-     * @return      a double value between 0 and 1
-     * @throws      Exception
+     * @return a double value between 0 and 1
+     * @throws Exception
      */
     public static double normalize(double min, double max, double value) throws Exception {
         return normalize(min, max, value, 0, 1);
@@ -128,16 +132,13 @@ public class Util {
      * @param min   lowest saturation point. Values below this will return 0
      * @param max   highest saturation point. Values above this will return 1
      * @param value
-     *
      * @param start beginning of the normalization interval
      * @param end   ending of the normalization interval
-     *
-     * @return      a double value between start and end
-     * @throws      Exception
-     *
+     * @return a double value between start and end
+     * @throws Exception
      */
     public static double normalize(double min, double max, double value,
-                                double start, double end) throws Exception {
+                                   double start, double end) throws Exception {
         if (max <= min) throw new Exception("Min cannot be higher than max. Values entered are not valid.");
         if (end <= start) throw new Exception("End cannot be higher than start. Values entered are not valid.");
         if (value >= max) return end;
@@ -152,51 +153,109 @@ public class Util {
 
     /**
      * float2Byte method - writes floats to byte array
-     * @param inData    float array
-     * @return          byte array
+     *
+     * @param inData float array
+     * @return byte array
      */
     public static byte[] float2Byte(float[] inData) {
-        int j=0;
-        int length=inData.length;
-        byte[] outData=new byte[length*4];
-        for (int i=0;i<length;i++) {
-            int data=Float.floatToIntBits(inData[i]);
-            outData[j++]=(byte)(data>>>24);
-            outData[j++]=(byte)(data>>>16);
-            outData[j++]=(byte)(data>>>8);
-            outData[j++]=(byte)(data>>>0);
+        int j = 0;
+        int length = inData.length;
+        byte[] outData = new byte[length * 4];
+        for (int i = 0; i < length; i++) {
+            int data = Float.floatToIntBits(inData[i]);
+            outData[j++] = (byte) (data >>> 24);
+            outData[j++] = (byte) (data >>> 16);
+            outData[j++] = (byte) (data >>> 8);
+            outData[j++] = (byte) (data >>> 0);
         }
         return outData;
     }
 
     /**
-     * Method to calculate standard deviation
-     * @param values    An array of doubles
-     * @return          Standard deviation - double
+     * Returns the maximum vlaue in an array
+     *
+     * @param array
+     * @return
      */
-    public static double stdDev(double [] values)
-    {
+    public static double max(double[] array) {
+        // Validates input
+        if (array == null) {
+            throw new IllegalArgumentException("The Array must not be null");
+        } else if (array.length == 0) {
+            throw new IllegalArgumentException("Array cannot be empty.");
+        }
+
+        // Finds and returns max
+        double max = array[0];
+        for (int j = 1; j < array.length; j++) {
+            if (Double.isNaN(array[j])) {
+                return Double.NaN;
+            }
+            if (array[j] > max) {
+                max = array[j];
+            }
+        }
+
+        return max;
+    }
+
+    /**
+     * Returns the minimum value in an array
+     *
+     * @param array
+     * @return
+     */
+    public static double min(double[] array) {
+        // Validates input
+        if (array == null) {
+            throw new IllegalArgumentException("The Array must not be null");
+        } else if (array.length == 0) {
+            throw new IllegalArgumentException("Array cannot be empty.");
+        }
+
+        // Finds and returns max
+        double min = array[0];
+        for (int j = 1; j < array.length; j++) {
+            if (Double.isNaN(array[j])) {
+                return Double.NaN;
+            }
+            if (array[j] < min) {
+                min = array[j];
+            }
+        }
+
+        return min;
+    }
+
+    /**
+     * Method to calculate standard deviation
+     *
+     * @param values An array of doubles
+     * @return Standard deviation - double
+     */
+    public static double stdDev(double[] values) {
         return Math.sqrt(variance(values));
     }
 
     /**
      * Method to calculate variance
-     * @param values    An array of doubles
-     * @return          Variance - double
+     *
+     * @param values An array of doubles
+     * @return Variance - double
      */
-    public static double variance(double [] values)
-    {
+    public static double variance(double[] values) {
         double mean = mean(values);
         double temp = 0;
-        for(double a :values)
-            temp += (mean-a)*(mean-a);
-        return temp/values.length;
+        for (double a : values)
+            temp += (mean - a) * (mean - a);
+        return temp / values.length;
     }
 
     /**
      * bytearray2float method - converts bytes into a float
-     * @param b     byte array
-     * @return      float value
+     *
+     * @param b byte array
+     * @return float value
      */
     public static float bytearray2float(byte[] b) {
         ByteBuffer buf = ByteBuffer.wrap(b);
